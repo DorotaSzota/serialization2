8a5cbede92f593a407632008586e13702495418d
szotad <szotad@SCSWL4085>
2023-05-22 17:27:40 +0200

    Dodaj pliki projektów.

---
+++ serialization2/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace serialization2
+{
+    [DataContract]
+    internal class Product 
+    {
+        [DataMember] public string Name { get; set; }
+        [DataMember] public decimal Price { get; set; }
+        [DataMember] public string Description { get; set; }
+
+        public Product(){}
+        public Product(string name, decimal price, string description)
+        {
+            Name = name; Price = price; Description = description;
+        }
+        public override string ToString() => $"{this.Name}, {this.Price}, {this.Description}";
+
+        
+    }
+}
---
+++ serialization2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+using System.Xml;
+
+namespace serialization2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            { new Product("Apple", 2.5m, "A fine fruit."),
+            new Product("Milk", 3.6m, "Skimmed milk"),
+            new Product("Sweet Bun", 1.6m, "Soft and sweet")
+            };
+
+            var ds = new DataContractSerializer(typeof(List<Product>));
+            using (var create = XmlWriter.Create("product.xml"))
+            {
+                ds.WriteObject(create, products);
+            }
+
+            List<Product> products2 = new List<Product>();
+            using (Stream str = File.OpenRead("product.xml")) 
+            products2 = (List<Product>)ds.ReadObject(str);
+
+            foreach(Product product in products2)
+            { Console.WriteLine(product.ToString()); }
+
+        }
+    }
+}
